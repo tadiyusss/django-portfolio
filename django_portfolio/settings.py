fc6b730baa9b7e595d8aae2fa3fee0d90f15ef5a
Thaddeus Toledo <toledothaddeusf@gmail.com>
2024-12-26 17:56:12 +0800

    added /static/ to ignored paths in djang-requests

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+import uuid
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5e_r#$d5!=tp!@9@+e5c_c8jssp*1hbt3_569mke@ejkz633of'
+SECRET_KEY = uuid.uuid4().hex
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -139,4 +140,5 @@ REQUEST_IGNORE_PATHS = (
     r'^admin/',
     r'^dashboard/',
     r'^favicon.ico',
+    r'^static/'
 )
