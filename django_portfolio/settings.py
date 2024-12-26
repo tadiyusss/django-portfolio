05b4c8bbd2a29b65ed67c16f6b1fa49289bd4f74
Thaddeus Toledo <toledothaddeusf@gmail.com>
2024-12-27 02:41:13 +0800

    Forgot to change debug mode and allowed host for production

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5e_r#$d5!=tp!@9@+e5c_c8jssp*1hbt3_569mke@ejkz633of'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
